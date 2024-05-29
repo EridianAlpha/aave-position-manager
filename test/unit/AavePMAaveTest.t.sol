f862a40dbf313de5f6d16eec1fd7b2fe97ba435c
Eridian <86061486+EridianAlpha@users.noreply.github.com>
2024-05-29 12:36:00 +0100

    Update Aave tests

--- test/unit/AavePMAaveTest.t.sol
+++ test/unit/AavePMAaveTest.t.sol
@@ -3,69 +3,34 @@ pragma solidity 0.8.24;
 
 import {AavePMTestSetup} from "test/unit/AavePMTestSetupTest.t.sol";
 
+import {IAavePM} from "src/interfaces/IAavePM.sol";
+
 // ================================================================
 // │                           AAVE TESTS                         │
 // ================================================================
-// TODO: Fix these tests
-// contract AavePMAaveTests is AavePMTestSetup {
-//     function test_SupplyWstETHToAave() public {
-//         vm.startPrank(manager1);
-//         // Send some ETH to the contract and wrap it to WETH
-//         (bool success,) = address(aavePM).call{value: SEND_VALUE}("");
-//         require(success, "Failed to send ETH to AavePM contract");
-//         aavePM.wrapETHToWETH();
+contract AavePMAaveTests is AavePMTestSetup {
+    function flashLoanSetup() public returns (address, uint256, uint256, bytes memory) {
+        address asset = makeAddr("USDC");
+        uint256 amount = 100;
+        uint256 premium = 2;
+        bytes memory params = abi.encode(asset, amount, premium);
+        return (asset, amount, premium, params);
+    }
 
-//         // Swap WETH for wstETH
-//         aavePM.swapTokens("wstETH/ETH", "ETH", "wstETH");
-//         uint256 wstETHbalanceBefore = wstETH.balanceOf(address(aavePM));
+    function test_FlashLoanRevertIfMsgSenderNotAavePool() public {
+        (address asset, uint256 amount, uint256 premium, bytes memory params) = flashLoanSetup();
 
-//         // Supply wstETH to Aave
-//         // TODO: Use external function to supply the collateral
-//         aavePM.aaveSupplyWstETH();
+        vm.startPrank(manager1);
+        vm.expectRevert(IAavePM.AaveFunctions__FlashLoanMsgSenderUnauthorized.selector);
+        aavePM.executeOperation(asset, amount, premium, attacker1, params);
+        vm.stopPrank();
+    }
 
-//         // Check the awstETH balance of the contract
-//         assertEq(awstETH.balanceOf(address(aavePM)), wstETHbalanceBefore);
-//         vm.stopPrank();
-//     }
+    function test_FlashLoanRevertIfInitiatorNotAavePM() public {
+        (address asset, uint256 amount, uint256 premium, bytes memory params) = flashLoanSetup();
 
-//     function test_AaveMaxHealthFactor() public {
-//         vm.startPrank(manager1);
-//         // Send some ETH to the contract and wrap it to WETH
-//         (bool success,) = address(aavePM).call{value: SEND_VALUE}("");
-//         require(success, "Failed to send ETH to AavePM contract");
-//         aavePM.wrapETHToWETH();
-
-//         // Swap WETH for wstETH
-//         aavePM.swapTokens("wstETH/ETH", "ETH", "wstETH");
-
-//         // Supply wstETH to Aave
-//         aavePM.aaveSupplyWstETH();
-
-//         (,,,,, uint256 healthFactor) = aavePM.getAaveAccountData();
-
-//         // Check the health factor is UINT256_MAX (Infinity) as nothing has been borrowed
-//         assertEq(healthFactor, UINT256_MAX);
-//         vm.stopPrank();
-//     }
-
-//     function test_AaveBorrowUSDC() public {
-//         vm.startPrank(manager1);
-//         // Send some ETH to the contract and wrap it to WETH
-//         (bool success,) = address(aavePM).call{value: SEND_VALUE}("");
-//         require(success, "Failed to send ETH to AavePM contract");
-//         aavePM.wrapETHToWETH();
-
-//         // Swap WETH for wstETH
-//         aavePM.swapTokens("wstETH/ETH", "ETH", "wstETH");
-
-//         // Supply wstETH to Aave
-//         aavePM.aaveSupplyWstETH();
-
-//         // Borrow USDC
-//         aavePM.aaveBorrowUSDC(USDC_BORROW_AMOUNT);
-
-//         // Check the USDC balance of the contract
-//         assertEq(USDC.balanceOf(address(aavePM)), USDC_BORROW_AMOUNT);
-//         vm.stopPrank();
-//     }
-// }
+        vm.startPrank(s_contractAddresses["aavePool"]);
+        vm.expectRevert(IAavePM.AaveFunctions__FlashLoanInitiatorUnauthorized.selector);
+        aavePM.executeOperation(asset, amount, premium, attacker1, params);
+    }
+}

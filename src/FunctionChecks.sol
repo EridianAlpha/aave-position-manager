2296233da4532e0396b806185c8816a1b2b36dfa
Eridian <86061486+EridianAlpha@users.noreply.github.com>
2024-06-15 11:19:21 +0100

    Reduce variables and imports

--- src/FunctionChecks.sol
+++ src/FunctionChecks.sol
@@ -5,13 +5,6 @@ pragma solidity 0.8.24;
 // │                           IMPORTS                            │
 // ================================================================
 
-// Inherited Contract Imports
-import {TokenSwaps} from "./TokenSwaps.sol";
-import {AaveFunctions} from "./AaveFunctions.sol";
-
-// Aave Imports
-import {IPool} from "@aave/aave-v3-core/contracts/interfaces/IPool.sol";
-
 // Interface Imports
 import {IAavePM} from "./interfaces/IAavePM.sol";
 
@@ -23,25 +16,22 @@ import {IAavePM} from "./interfaces/IAavePM.sol";
 contract FunctionChecks {
     /// @notice // TODO: Add comment
     function _checkOwner(address _owner) internal view {
-        IAavePM aavePM = IAavePM(address(this));
-        if (!aavePM.hasRole(keccak256("OWNER_ROLE"), _owner)) revert IAavePM.AavePM__AddressNotAnOwner();
+        if (!IAavePM(address(this)).hasRole(keccak256("OWNER_ROLE"), _owner)) {
+            revert IAavePM.AavePM__AddressNotAnOwner();
+        }
     }
 
     /// @notice // TODO: Add comment
     function _checkManagerInvocationLimit(uint64[] memory managerInvocations) internal view {
-        IAavePM aavePM = IAavePM(address(this));
-
         // If the array is smaller than getManagerDailyInvocationLimit, return
-        if (managerInvocations.length < aavePM.getManagerDailyInvocationLimit()) return;
-
-        uint256 cutoff = block.timestamp - 24 hours;
+        if (managerInvocations.length < IAavePM(address(this)).getManagerDailyInvocationLimit()) return;
 
         // Check if all invocations are in the past 24 hours, starting by assuming they all are
         bool allInvocationsInPast24Hours = true;
 
         // If any are not in the past 24 hours, set allInvocationsInPast24Hours to false and revert
         for (uint256 i = 0; i < managerInvocations.length; i++) {
-            if (managerInvocations[i] < cutoff) allInvocationsInPast24Hours = false;
+            if (managerInvocations[i] < (block.timestamp - 24 hours)) allInvocationsInPast24Hours = false;
         }
         if (allInvocationsInPast24Hours) revert IAavePM.AavePM__ManagerDailyInvocationLimitReached();
     }

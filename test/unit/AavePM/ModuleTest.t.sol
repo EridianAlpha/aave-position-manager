05f031531fa231daab24ff60153d022fac874782
Eridian <86061486+EridianAlpha@users.noreply.github.com>
2024-06-19 15:28:37 +0100

    Add constructor tests

--- test/unit/AavePM/ModuleTest.t.sol
+++ test/unit/AavePM/ModuleTest.t.sol
@@ -15,51 +15,71 @@ import {TokenSwapsModule} from "src/modules/TokenSwapsModule.sol";
 contract AaveFunctionsModuleTests is Test, AaveFunctionsModule {
     constructor() AaveFunctionsModule(address(this)) {}
 
-    AaveFunctionsModule aaveFunctionsModule = new AaveFunctionsModule(address(this));
-
     function test_GetVersion() public {
+        AaveFunctionsModule aaveFunctionsModule = new AaveFunctionsModule(address(this));
         assertEq(keccak256(abi.encodePacked(aaveFunctionsModule.getVersion())), keccak256(abi.encodePacked(VERSION)));
+    }
+
+    function test_CheckAavePMProxyAddress() public {
+        AaveFunctionsModule aaveFunctionsModule = new AaveFunctionsModule(address(this));
+        assertEq(aaveFunctionsModule.aavePMProxyAddress(), address(this));
     }
 }
 
 contract BorrowAndWithdrawUSDCModuleTests is Test, BorrowAndWithdrawUSDCModule {
     constructor() BorrowAndWithdrawUSDCModule(address(this)) {}
 
-    BorrowAndWithdrawUSDCModule borrowAndWithdrawUSDCModule = new BorrowAndWithdrawUSDCModule(address(this));
-
     function test_GetVersion() public {
+        BorrowAndWithdrawUSDCModule borrowAndWithdrawUSDCModule = new BorrowAndWithdrawUSDCModule(address(this));
         assertEq(
             keccak256(abi.encodePacked(borrowAndWithdrawUSDCModule.getVersion())), keccak256(abi.encodePacked(VERSION))
         );
+    }
+
+    function test_CheckAavePMProxyAddress() public {
+        BorrowAndWithdrawUSDCModule borrowAndWithdrawUSDCModule = new BorrowAndWithdrawUSDCModule(address(this));
+        assertEq(borrowAndWithdrawUSDCModule.aavePMProxyAddress(), address(this));
     }
 }
 
 contract RebalanceModuleTests is Test, RebalanceModule {
     constructor() RebalanceModule(address(this)) {}
 
-    RebalanceModule rebalanceModule = new RebalanceModule(address(this));
-
     function test_GetVersion() public {
+        RebalanceModule rebalanceModule = new RebalanceModule(address(this));
         assertEq(keccak256(abi.encodePacked(rebalanceModule.getVersion())), keccak256(abi.encodePacked(VERSION)));
+    }
+
+    function test_CheckAavePMProxyAddress() public {
+        RebalanceModule rebalanceModule = new RebalanceModule(address(this));
+        assertEq(rebalanceModule.aavePMProxyAddress(), address(this));
     }
 }
 
 contract ReinvestModuleTests is Test, ReinvestModule {
     constructor() ReinvestModule(address(this)) {}
 
-    ReinvestModule reinvestModule = new ReinvestModule(address(this));
-
     function test_GetVersion() public {
+        ReinvestModule reinvestModule = new ReinvestModule(address(this));
         assertEq(keccak256(abi.encodePacked(reinvestModule.getVersion())), keccak256(abi.encodePacked(VERSION)));
+    }
+
+    function test_CheckAavePMProxyAddress() public {
+        ReinvestModule reinvestModule = new ReinvestModule(address(this));
+        assertEq(reinvestModule.aavePMProxyAddress(), address(this));
     }
 }
 
 contract TokenSwapsModuleTests is Test, TokenSwapsModule {
     constructor() TokenSwapsModule(address(this)) {}
 
-    TokenSwapsModule tokenSwapsModule = new TokenSwapsModule(address(this));
-
     function test_GetVersion() public {
+        TokenSwapsModule tokenSwapsModule = new TokenSwapsModule(address(this));
         assertEq(keccak256(abi.encodePacked(tokenSwapsModule.getVersion())), keccak256(abi.encodePacked(VERSION)));
+    }
+
+    function test_CheckAavePMProxyAddress() public {
+        TokenSwapsModule tokenSwapsModule = new TokenSwapsModule(address(this));
+        assertEq(tokenSwapsModule.aavePMProxyAddress(), address(this));
     }
 }

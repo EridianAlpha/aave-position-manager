da991a305ddbb93264530b4160032134932a7a08
Eridian <86061486+EridianAlpha@users.noreply.github.com>
2024-06-19 15:59:20 +0100

    Add CheckAavePMProxyAddressFail tests for all public module functions

--- test/unit/AavePM/ModuleTest.t.sol
+++ test/unit/AavePM/ModuleTest.t.sol
@@ -3,10 +3,19 @@ pragma solidity 0.8.24;
 
 import {Test, console} from "forge-std/Test.sol";
 
+import {IAaveFunctionsModule} from "src/interfaces/IAaveFunctionsModule.sol";
 import {AaveFunctionsModule} from "src/modules/AaveFunctionsModule.sol";
+
+import {IBorrowAndWithdrawUSDCModule} from "src/interfaces/IBorrowAndWithdrawUSDCModule.sol";
 import {BorrowAndWithdrawUSDCModule} from "src/modules/BorrowAndWithdrawUSDCModule.sol";
+
+import {IRebalanceModule} from "src/interfaces/IRebalanceModule.sol";
 import {RebalanceModule} from "src/modules/RebalanceModule.sol";
+
+import {IReinvestModule} from "src/interfaces/IReinvestModule.sol";
 import {ReinvestModule} from "src/modules/ReinvestModule.sol";
+
+import {ITokenSwapsModule} from "src/interfaces/ITokenSwapsModule.sol";
 import {TokenSwapsModule} from "src/modules/TokenSwapsModule.sol";
 
 // ================================================================
@@ -24,6 +33,25 @@ contract AaveFunctionsModuleTests is Test, AaveFunctionsModule {
         AaveFunctionsModule aaveFunctionsModule = new AaveFunctionsModule(address(this));
         assertEq(aaveFunctionsModule.aavePMProxyAddress(), address(this));
     }
+
+    function test_CheckAavePMProxyAddressFail() public {
+        AaveFunctionsModule aaveFunctionsModule = new AaveFunctionsModule(address(this));
+
+        vm.expectRevert(IAaveFunctionsModule.AaveFunctionsModule__InvalidAavePMProxyAddress.selector);
+        aaveFunctionsModule.aaveSupply(makeAddr("test"), makeAddr("test"), 0);
+
+        vm.expectRevert(IAaveFunctionsModule.AaveFunctionsModule__InvalidAavePMProxyAddress.selector);
+        aaveFunctionsModule.aaveWithdrawCollateral(makeAddr("test"), makeAddr("test"), 0);
+
+        vm.expectRevert(IAaveFunctionsModule.AaveFunctionsModule__InvalidAavePMProxyAddress.selector);
+        aaveFunctionsModule.aaveBorrow(makeAddr("test"), makeAddr("test"), 0);
+
+        vm.expectRevert(IAaveFunctionsModule.AaveFunctionsModule__InvalidAavePMProxyAddress.selector);
+        aaveFunctionsModule.aaveRepayDebt(makeAddr("test"), makeAddr("test"), 0);
+
+        vm.expectRevert(IAaveFunctionsModule.AaveFunctionsModule__InvalidAavePMProxyAddress.selector);
+        aaveFunctionsModule.convertExistingBalanceToWstETHAndSupplyToAave();
+    }
 }
 
 contract BorrowAndWithdrawUSDCModuleTests is Test, BorrowAndWithdrawUSDCModule {
@@ -40,6 +68,13 @@ contract BorrowAndWithdrawUSDCModuleTests is Test, BorrowAndWithdrawUSDCModule {
         BorrowAndWithdrawUSDCModule borrowAndWithdrawUSDCModule = new BorrowAndWithdrawUSDCModule(address(this));
         assertEq(borrowAndWithdrawUSDCModule.aavePMProxyAddress(), address(this));
     }
+
+    function test_CheckAavePMProxyAddressFail() public {
+        BorrowAndWithdrawUSDCModule borrowAndWithdrawUSDCModule = new BorrowAndWithdrawUSDCModule(address(this));
+
+        vm.expectRevert(IBorrowAndWithdrawUSDCModule.BorrowAndWithdrawUSDCModule__InvalidAavePMProxyAddress.selector);
+        borrowAndWithdrawUSDCModule.borrowAndWithdrawUSDC(0, makeAddr("test"));
+    }
 }
 
 contract RebalanceModuleTests is Test, RebalanceModule {
@@ -53,6 +88,13 @@ contract RebalanceModuleTests is Test, RebalanceModule {
     function test_CheckAavePMProxyAddress() public {
         RebalanceModule rebalanceModule = new RebalanceModule(address(this));
         assertEq(rebalanceModule.aavePMProxyAddress(), address(this));
+    }
+
+    function test_CheckAavePMProxyAddressFail() public {
+        RebalanceModule rebalanceModule = new RebalanceModule(address(this));
+
+        vm.expectRevert(IRebalanceModule.RebalanceModule__InvalidAavePMProxyAddress.selector);
+        rebalanceModule.rebalance();
     }
 }
 
@@ -68,6 +110,13 @@ contract ReinvestModuleTests is Test, ReinvestModule {
         ReinvestModule reinvestModule = new ReinvestModule(address(this));
         assertEq(reinvestModule.aavePMProxyAddress(), address(this));
     }
+
+    function test_CheckAavePMProxyAddressFail() public {
+        ReinvestModule reinvestModule = new ReinvestModule(address(this));
+
+        vm.expectRevert(IReinvestModule.ReinvestModule__InvalidAavePMProxyAddress.selector);
+        reinvestModule.reinvest();
+    }
 }
 
 contract TokenSwapsModuleTests is Test, TokenSwapsModule {
@@ -81,5 +130,15 @@ contract TokenSwapsModuleTests is Test, TokenSwapsModule {
     function test_CheckAavePMProxyAddress() public {
         TokenSwapsModule tokenSwapsModule = new TokenSwapsModule(address(this));
         assertEq(tokenSwapsModule.aavePMProxyAddress(), address(this));
+    }
+
+    function test_CheckAavePMProxyAddressFail() public {
+        TokenSwapsModule tokenSwapsModule = new TokenSwapsModule(address(this));
+
+        vm.expectRevert(ITokenSwapsModule.TokenSwapsModule__InvalidAavePMProxyAddress.selector);
+        tokenSwapsModule.swapTokens("USDC/ETH", "USDC", "ETH");
+
+        vm.expectRevert(ITokenSwapsModule.TokenSwapsModule__InvalidAavePMProxyAddress.selector);
+        tokenSwapsModule.wrapETHToWETH();
     }
 }
